e8584b4bf2aa7cdf6795618b517dd55b97435b28
ngyngcphu <nguyenngocphu225@gmail.com>
2023-04-17 15:48:35 +0700

    complete rr

---
+++ scheduling_algorithms/schedule_rr.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "schedulers.h"
+#include "list.h"
+#include "cpu.h"
+
+struct node *taskList = NULL;
+
+void add(char *name, int priority, int burst)
+{
+    Task *newTask = malloc(sizeof(Task));
+    newTask->name = malloc(sizeof(char) * (strlen(name) + 1));
+    strcpy(newTask->name, name);
+    newTask->priority = priority;
+    newTask->burst = newTask->remaining_burst = burst;
+    insert(&taskList, newTask);
+}
+
+Task *getNextTask()
+{
+    struct node *lastTask = taskList;
+    while (lastTask->next)
+    {
+        lastTask = lastTask->next;
+    }
+    return lastTask->task;
+}
+
+void schedule()
+{
+    while (taskList)
+    {
+        Task *runTask = getNextTask();
+        int slice = QUANTUM < runTask->remaining_burst ? QUANTUM : runTask->remaining_burst;
+        run(runTask, slice);
+        runTask->remaining_burst -= slice;
+        if (runTask->remaining_burst)
+        {
+            delete (&taskList, runTask);
+            insert(&taskList, runTask);
+        }
+        else
+        {
+            delete(&taskList, runTask);
+            free(runTask->name);
+            free(runTask);
+        }
+    }
+}
